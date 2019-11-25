a6f822538a3ee0b5ecedf1c956d44af8abac049c
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 09:04:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,9 +18,12 @@ def using_shift(array)
   array.shift
 end
 
-def shift_with_args(2)
+def shift_with_args(array)
   array.shift(2)
 end
+
+def using_concat(array1,array2)
+  
 
 def using_insert(array,element)
   array1.insert(4,element)
